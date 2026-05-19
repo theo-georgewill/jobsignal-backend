4a6863018b09504c5fb0f04bfdda51cc9ce62af1
Theo <ttggwll@gmail.com>
2026-05-19 11:01:41 +0100

    feat: setup queues using bullmq

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-
+import { QueuesModule } from '../queues/queues.module';
+import { CompanyProcessor } from './company.processor';
+import { CompanyEnrichmentService } from './services/company-enrichment.service';
 @Module({
-  imports: [PrismaModule],
-  providers: [CompanyService],
-  controllers: [CompanyController]
+  imports: [PrismaModule, QueuesModule],
+  providers: [CompanyService, CompanyProcessor, CompanyEnrichmentService],
+  controllers: [CompanyController],
+  exports: [CompanyService],
 })
 export class CompanyModule {}
