8ae8b92da446fb43c751fc685ebf9a464beb4b1d
Theo <ttggwll@gmail.com>
2026-06-04 13:08:35 +0100

    update company queue

---
+++ src/ingestion/queues/company.queue.ts
@@ -0,0 +1,56 @@
+import { InjectQueue } from '@nestjs/bullmq';
+import { Injectable } from '@nestjs/common';
+import { Queue } from 'bullmq';
+
+@Injectable()
+export class CompanyQueue {
+  constructor(
+    @InjectQueue('company')
+    private queue: Queue,
+  ) {}
+
+  private getJobOptions() {
+    return {
+      attempts: 3,
+
+      backoff: {
+        type: 'exponential',
+        delay: 5000,
+      },
+
+      removeOnComplete: 1000,
+
+      removeOnFail: 5000,
+    };
+  }
+
+  async enrich(companyId: string) {
+    await this.queue.add(
+      'enrich-company',
+      {
+        companyId,
+      },
+      {
+        attempts: 3,
+
+        backoff: {
+          type: 'exponential',
+          delay: 5000,
+        },
+
+        removeOnComplete: 1000,
+        removeOnFail: 5000,
+      },
+    );
+  }
+
+  async checkCareerPage(
+    companyId: string,
+  ) {
+    await this.queue.add(
+      'check-career-page',
+      { companyId },
+      this.getJobOptions(),
+    );
+  }
+}
