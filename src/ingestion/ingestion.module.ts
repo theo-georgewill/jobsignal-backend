bc036d31ec7d88d576380c6330861c8da6e448da
Theo <ttggwll@gmail.com>
2026-06-07 13:09:19 +0100

    update ingestion module

--- src/ingestion/ingestion.module.ts
+++ src/ingestion/ingestion.module.ts
@@ -4,7 +4,7 @@ import { JobsModule } from '../jobs/jobs.module';
 import { IngestionController } from './ingestion.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SignalsModule } from '../signals/signals.module';
-import { QueuesModule } from '../queues/queues.module';
+import { QueuesModule } from './queues/queues.module';
 @Module({
   imports: [JobsModule, PrismaModule, SignalsModule, QueuesModule],
   providers: [IngestionService],
